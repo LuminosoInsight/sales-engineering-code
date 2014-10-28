79b3bc3b22ddc88da075d316837d758346aeea5f
Tim Lewandowski <tlewandowski@luminoso.com>
2014-10-28 13:38:48 -0400

    Setup CLI

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="Sales Engineering Code",
     version = '0.1',
-    maintainer='Luminoso, LLC',
+    maintainer='Luminoso Technologies, Inc.',
     maintainer_email='dev@lumino.so',
     license = "LICENSE",
     url = 'http://github.com/LuminosoInsight/sales-engineering-code',
@@ -12,7 +12,10 @@ setup(
                    "will be given to customers"),
     packages=find_packages(),
     install_requires=[
+        'luminoso_api'
         ],
     entry_points={
-    },
+    'console_scripts': [
+        'topic_copier = se_code.topic_copier:main'
+    ]},
 )
