3baaf9d9a0e5c9ebbc7e981ce3a4d9959a6f69d1
Tim Lewandowski <tlewandowski@luminoso.com>
2015-03-16 17:38:28 -0400

    Process each document individually, rather than downloading them all at once

--- se_code/corr_export.py
+++ se_code/corr_export.py
@@ -14,23 +14,6 @@ PROJECT_PATH = 'projects/%s/%s' % (ACCOUNT_ID, PROJECT_ID)
 project = LuminosoClient.connect(PROJECT_PATH, username=USERNAME)
 DOC_FIELDS = ['title', 'text', '_id']
 
-def download_docs(project, doc_fields=DOC_FIELDS):
-    """
-    Given a project, download all the documents from it. There is NO SANITY
-    CHECKING for project size, so this should only be used on relatively small
-    (<100K docs) projects.
-    """
-    i = 0
-    docs = []
-    while True:
-        batch = project.get('docs', limit=25000, offset=i, 
-                            doc_fields=doc_fields)
-        if batch == []:
-            break
-        docs += batch
-        i += len(batch)
-    return docs
-
 
 def doc_to_row(doc, corrs, topics):
     """
@@ -43,11 +26,20 @@ def doc_to_row(doc, corrs, topics):
     return [doc['title'], doc['text']] + doc_corrs
 
 
-docs = download_docs(project)
 topics = project.get('topics')
 corrs = project.get('docs/correlations')
 header = [['title', 'text'] + [topic['name'] for topic in topics]]
-rows = header + [doc_to_row(doc, corrs, topics) for doc in docs]
 
 with open(project.get()['name'] + '_corrs.csv', 'w') as f:
-    csv.writer(f).writerows(rows)
+    writer = csv.writer(f)
+    writer.writerow(header)
+    i = 0
+    docs = []
+    while True:
+        batch = project.get('docs', limit=25000, offset=i, 
+                            doc_fields=DOC_FIELDS)
+        if batch == []:
+            break
+        i += len(batch)
+        for doc in batch:
+            writer.writerow(doc_to_row(doc, corrs, topics))
