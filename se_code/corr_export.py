1edac57a0058671cde6d1c93b0b101c46bf5078c
Tim Lewandowski <tlewandowski@luminoso.com>
2015-03-17 12:54:25 -0400

    Use project_info

--- se_code/corr_export.py
+++ se_code/corr_export.py
@@ -6,9 +6,9 @@ This script is essentially a wrapper for the GET /docs/correlations/ endpoint,
 and outputs a human-readable CSV with topic-document correlations.
 """
 
-ACCOUNT_ID = 'a11a111a'
-PROJECT_ID = 'a1a1a'
-USERNAME = 'user@luminoso.com'
+ACCOUNT_ID = 'lumi-test'
+PROJECT_ID = 'qg7wb'
+USERNAME = 'tlewandowski@luminoso.com'
 PROJECT_PATH = 'projects/%s/%s' % (ACCOUNT_ID, PROJECT_ID)
 
 project = LuminosoClient.connect(PROJECT_PATH, username=USERNAME)
@@ -25,20 +25,19 @@ def doc_to_row(doc, corrs, topics):
     return [doc.get('title'), doc['text']] + doc_corrs
 
 
+project_info = project.get()
 topics = project.get('topics')
 corrs = project.get('docs/correlations')
 header = [['title', 'text'] + [topic['name'] for topic in topics]]
 
-with open(project.get()['name'] + '_corrs.csv', 'w') as f:
+with open(project_info['name'] + '_corrs.csv', 'w') as f:
     writer = csv.writer(f)
     writer.writerow(header)
     i = 0
     docs = []
-    while True:
+    while i < project_info['counts']['__all__']:
         batch = project.get('docs', limit=25000, offset=i,
                             doc_fields=DOC_FIELDS)
-        if batch == []:
-            break
         i += len(batch)
         for doc in batch:
             writer.writerow(doc_to_row(doc, corrs, topics))
