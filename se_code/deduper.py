11944c7d2b246c65cc57dcf5ca0f7e305182dd82
Tim O'Brien <tobrien6@gmail.com>
2015-07-09 16:12:08 -0400

    Reduced most lines to < 80 characters and other minor changes.

--- se_code/deduper.py
+++ se_code/deduper.py
@@ -5,25 +5,29 @@ import networkx
 from networkx.algorithms.components.connected import connected_components
 
 class Deduper(object):
-    """ This class is initialized by pointing it at a project. It then downloads all of the documents,
-        vectorizes them based on unprocessed surface forms, finds the pairwise cosine similarity,
-        groups documents in to sets of highly similar documents, and then deletes all but one from the project.
+    """ This class is initialized by pointing it at a project. It then downloads
+        all of the documents, vectorizes them based on unprocessed surface forms,
+        finds the pairwise cosine similarity, groups documents in to sets of highly
+        similar documents, and then deletes all but one from the project.
         
-        The choice of which one to delete (or to create and retain a new 'hybrid' document) is left to the 
-        reconcile function, which can also be specified upon initialization. Since the complexity is n^2
-        (more precisely n choose 2), the split_amt parameter allows the deduplication process to be batched
-        for large projects. Note that this batching may cause a few dupes to be missed, and is only
-        effective for projects with dates that have the property of dupes being likely to occur close to each
-        other in time. In practice, the data sets that have dupes have been small enough to do in one batch, but
-        when that changes the batching algorithm could be improved or the entire thing could be overhauled
-        to use LSH which should reduce the complexity to n*log(n).
+        The choice of which one to delete (or to create and retain a new 'hybrid'
+        document) is left to the reconcile function, which can also be specified
+        upon initialization. Since the complexity is n^2 (more precisely n choose 2),
+        the split_amt parameter allows the deduplication process to be batched
+        for large projects. Note that this batching may cause a few dupes to be
+        missed, and is only effective for projects with dates that have the property
+        of dupes being likely to occur close to each other in time. In practice,
+        the data sets that have dupes have been small enough to do in one batch, but
+        when that changes the batching algorithm could be improved or the entire thing
+        could be overhauled to use LSH which should reduce the complexity to n*log(n).
         
-        If you wish to use a custom reconcile function, you should pass a function that takes a list of
-        document dictionaries, and returns a single document dictionary to be retained in the project. """
+        If you wish to use a custom reconcile function, you should pass a function
+        that takes a list of document dictionaries, and returns a single document
+        dictionary to be retained in the project. """
 
-    def __init__(self, acct, proj, token, split_amt=10**10, reconcile_func=None, sim_threshhold = 0.95):
+    def __init__(self, acct, proj, token, split_amt=10**10, reconcile_func=None, thresh=0.95):
         """ split_amt is the number of docs per batch  """
-        self.thresh = sim_threshhold
+        self.thresh = thresh
         self.reconcile_func = reconcile_func
         self.split_amt = split_amt
         self.cli = LuminosoClient.connect('/projects/'+acct+'/'+proj, token=token)
@@ -45,19 +49,20 @@ class Deduper(object):
         return [(i, min(i + interval, maximum)) for i in range(0, maximum, interval)]
 
     def get_similar(self, similarity_matrix):
-        """ Return all pairs with similarity > self.thresh. This is batched to save memory """
+        """ Return all pairs with similarity > self.thresh.
+            This is batched to save memory """
         similar = []
         for lower, upper in self.intervals(similarity_matrix.shape[0], 5000):
             sim = similarity_matrix[lower:upper,] > self.thresh
             sim = sim.nonzero()
-            similar.extend([(x[0]+lower, x[1]) for x in zip(*sim)])
+            similar.extend([(x[0] + lower, x[1]) for x in zip(*sim)])
             print("processed rows " + str(lower) + " to row " + str(upper))
-        return [s for s in similar if s[0]!=s[1]]
+        return [s for s in similar if s[0] != s[1]]
 
-    # get_similar outputs a list of dupe pairs. The following graph methods turn these
-    # dupe pairs in to a graph and return the connected components (full sets of dupes).
-    # Note: in some cases two docs will be considered dupes even with similarity a bit < sim.thresh
-    #       due to transitivity
+    # get_similar outputs a list of dupe pairs. The following graph methods turn
+    # these dupe pairs in to a graph and return the connected components (full
+    # sets of dupes). Note: in some cases two docs will be considered dupes even
+    # with similarity a bit less than self.thresh due to transitivity
     def to_graph(self, l):
         G = networkx.Graph()
         for part in l:
@@ -82,8 +87,7 @@ class Deduper(object):
         or create a new document according to some rules. """
         if self.reconcile_func:
             return self.reconcile_func(dupes)
-        else:
-            return dupes[0]
+        return dupes[0]
 
     def chunks(self, l, n):
         """ Partitions a list in to a list of lists """
@@ -99,11 +103,11 @@ class Deduper(object):
 
         if self.split_amt < n_docs:
             try:
-                documents = sorted(documents, key = lambda d: d['date'])
+                documents = sorted(documents, key=lambda d: d['date'])
             except KeyError:
-                print("""WARNING: The deduplication process is being batched, but your
-                data does not contain a date field. Thus it is likely that many duplicates are
-                being missed across batches.""")
+                print("""WARNING: The deduplication process is being batched,
+                but your data does not contain a date field. Thus it is likely
+                that many duplicates are being missed across batches.""")
 
         batches = self.chunks(documents, self.split_amt)
 
@@ -114,7 +118,7 @@ class Deduper(object):
         for i,batch in enumerate(batches):
             print("Starting batch " + str(i+1) + " of " + str(len(batches)))
 
-            #want to vectorize based on surface forms for duplicate detection
+            # want to vectorize based on surface forms for duplicate detection
             tfidf = TfidfVectorizer().fit_transform([d['text'] for d in batch])
             print("Finished vectorizing documents")
 
@@ -125,28 +129,31 @@ class Deduper(object):
             sets_of_dupes = self.get_dupes(self.get_similar(pairwise_similarity))
             print("Finished identifying duplicates")
 
-            #get duplicates and near-duplicates and reconcile them. Deduped is a list of docs to keep
-            dupe_ids = [batch[i]['_id'] for i in chain(*sets_of_dupes)] #IDs of all dupes
+            # get duplicates and near-duplicates and reconcile them.
+            # Deduped is a list of docs to keep
+            dupe_ids = [batch[i]['_id'] for i in chain(*sets_of_dupes)]
 
             deduped = [] #will contain the duplicates that should be retained
             for dupe_set in sets_of_dupes:
                 docs = [batch[i] for i in dupe_set]
                 deduped.append(self.reconcile_dupes(docs))
 
-            dupe_ids_partitions = self.chunks(dupe_ids, 100) #partitioned due to URI length limitations
+            #partitioned due to URI length limitations
+            dupe_ids_partitions = self.chunks(dupe_ids, 100)
 
-            #send delete request to delete all dupes
+            # send delete request to delete all dupes
             for d in dupe_ids_partitions:
                 self.cli.delete('/docs', ids=d)
             print("Finished deleting duplicates from project \n")
 
-            #need to remove the _id field otherwise it will get deleted upon recalc (if it's the same
-            #as one of the deleted docs, which it often is depending on the reconcile function).
+            # need to remove the _id field otherwise it will get deleted
+            # upon recalc (if it's the same as one of the deleted docs,
+            # which it often is depending on the reconcile function).
             for d in deduped:
                 if '_id' in d:
                     del d['_id']
 
-            #upload the dupes we have chosen to keep
+            # upload the dupes we have chosen to keep
             self.cli.upload('docs', deduped)
         
         print("Deduping finished. Project is now recalculating.")
