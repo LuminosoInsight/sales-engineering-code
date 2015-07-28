8aa0477128d2e69ae410db31b3600a5fe4c6d79f
Tim O'Brien <tobrien6@gmail.com>
2015-07-28 13:24:22 -0400

    basic routes in place

--- api_utils_app/web_app.py
+++ api_utils_app/web_app.py
@@ -1,21 +1,36 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, session, url_for
 import json
 from collections import defaultdict, OrderedDict
 from luminoso_api import LuminosoClient
 
 app = Flask(__name__)
+#app.secret_key = 'some_secret'
 
 @app.route('/')
-def index():
+def home():
     return render_template('login.html')
 
-@app.route('/apps')
-def show_apps():
+@app.route('/login', methods=['POST'])
+def login():
     """From the given token, get name and id of surveys"""
-    return render_template('apps.html')
-    #token = request.args.get('token', 0, type=str)
-    #info = get_name_id(token) #returns a dictionary
-    #return jsonify(**info)
+    session['username'] = request.form['username']
+    session['password'] = request.form['password']
+    session['apps_to_show'] = {'app1':url_for('app1')}
+    try:
+        LuminosoClient.connect('/projects/', username=session['username'],
+                                             password=session['password'])
+        return redirect(url_for('index'))
+    except:
+        error = 'Invalid credentials'
+        return render_template('login.html', error=error)
+
+@app.route('/index')
+def index():
+    return render_template('index.html', apps=session['apps_to_show'])
+
+@app.route('/app1')
+def app1():
+	return render_template('lumi_app1.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
