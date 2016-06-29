0ef6e403e24240c3318e4621defe0e28ed12ad52
Tim Lewandowski <tlewandowski@luminoso.com>
2016-06-29 18:13:52 -0400

    Use a usable default when downloading docs

--- se_code/doc_downloader.py
+++ se_code/doc_downloader.py
@@ -9,7 +9,8 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def download_docs(project, batch_size=25000, doc_fields=None):
+def download_docs(project, batch_size=25000,
+                  doc_fields=('title', 'text', 'subsets', 'predict', 'date')):
     """
     Download all of the documents from a given project, given a LuminosoClient
     object pointed at that project.
@@ -88,8 +89,6 @@ def main():
         docs = download_docs(project, doc_fields=['text'])
         write_csv(docs, filename)
     else:
-        docs = download_docs(
-            project, doc_fields=['title', 'text', 'subsets', 'predict', 'date']
-        )
+        docs = download_docs(project)
         write_jsons(docs, filename)
     logger.info('Wrote docs to %s' % filename)
