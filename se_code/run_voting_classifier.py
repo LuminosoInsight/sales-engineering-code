3b3a55fb39a5ade683383b52ba45c1a1bb1c5d8f
sheyvaert <sheyvaert@lumino.so>
2017-04-04 16:05:00 -0400

    Use code from ne voting_classifier module
    - rename `voting_classifier.py` to `run_voting_classifier.py` to avoid conflict
    - import all math and text-processing functionality from voting_classifier module
    
    The test with the Fidelity COSMO_SEMANTIC_TAG subset reports 81.04% overall accuracy.

--- se_code/run_voting_classifier.py
+++ se_code/run_voting_classifier.py
@@ -2,25 +2,10 @@ import argparse
 import csv
 import numpy as np
 import json
-import math
-from sklearn.svm import LinearSVC
-from sklearn.preprocessing import normalize
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 
 from luminoso_api import LuminosoClient
-from pack64 import pack64, unpack64
-
-
-def sigmoid(x):
-    '''
-    Map distance to hyperplane to a range 0-1.
-    Approximation of "likelihood" that the prediction is correct.
-    x being a list of lists:
-        list of the output the from decision_function for each sample
-    '''
-
-    return [[1 / (1 + math.exp(-y)) for y in z] for z in x]
+from voting_classifier.util import train_classifier, classify_documents
 
 
 def extract_labels(labels):
@@ -38,45 +23,6 @@ def extract_labels(labels):
             raise ValueError("Subset prefix must contain colon between prefix and value.")
 
     return extracted_labels
-
-
-def combine_decision_functions(cls_dfuncs, classes, weights=None):
-    '''
-    Combine outputs from multiple classifiers. Apply weights.
-    Take mean of result across classifiers.
-    The weighting ability of this function has yet to show
-    meaningful improvements in accuracy... remove?
-    '''
-
-    if classes == 2:
-        cls_dfuncs = [[(-b, b) for b in a] for a in cls_dfuncs]
-
-    if weights:
-        cls_dfuncs = [c * w for c, w in zip(cls_dfuncs, weights)]
-
-    classification = np.dstack(cls_dfuncs)
-
-    classification = np.mean(classification, axis=2)
-
-    return sigmoid(classification)
-
-
-def sklearn_text(termlist, lang='en'):
-    '''
-    Convert a list of Luminoso terms, possibly multi-word terms, into text that
-    the tokenizer we get from `make_term_vectorizer` below will tokenize into
-    those terms.
-
-    Yes the tokenizer will basically be undoing what this function does, but it
-    means we also get the benefit of sklearn's TF-IDF.
-    '''
-    langtag = '|' + lang
-    fixed_terms = [
-        term.replace(langtag, '').replace(' ', '_')
-        for term, _tag, _span in termlist
-        if '\N{PILCROW SIGN}' not in term
-    ]
-    return ' '.join(fixed_terms)
 
 
 def get_all_docs(client, subset_field, batch_size=20000):
@@ -124,79 +70,6 @@ def split_train_test(docs, labels, split=0.3):
         labels = [labels[i] for i in indexes]
         docs = [docs[i] for i in indexes]
     return train_test_split(np.array(docs), np.array(labels), test_size=split, random_state=32, stratify=np.array(labels))
-
-
-def make_term_vectorizer():
-    '''
-    Return a sklearn vectorizer whose tokenizer only splits on whitespace.
-    This is for text that we have already tokenized, in the Luminoso way, and
-    then stuck together with whitespace.
-    '''
-
-    # Consider using min_df?
-    return TfidfVectorizer(
-                           sublinear_tf=True,
-                           max_df=0.5,
-                           stop_words=None,
-                           token_pattern=r'\S+'
-                           )
-
-
-def make_simple_vectorizer():
-    '''
-    Return a sklearn vectorizer that does sklearn's usual thing with arbitrary
-    English text.
-    '''
-
-    # Consider using min_df?
-    return TfidfVectorizer(
-                           sublinear_tf=True,
-                           max_df=0.5,
-                           stop_words='english'
-                           )
-
-
-def train_classifier(client, train_docs, train_labels):
-    '''
-    Train a classifier.
-
-    Input: a list of training documents, and a list of labels.
-    Output: a pair of (classifiers, vectorizers).
-
-    The returned items represent three different sklearn classifiers and their
-    corresponding vectorizers.
-    These should be passed on to the `classify_test_documents` function.
-    '''
-
-    if len(train_docs) == 0:
-        raise ValueError("Training documents cannot be empty")
-
-    term_vectorizer = make_term_vectorizer()
-    simple_vectorizer = make_simple_vectorizer()
-    vectorizers = {'simple': simple_vectorizer, 'term': term_vectorizer}
-
-    classifiers = {
-        style: LinearSVC(
-            C=1.0, class_weight=None, dual=False, fit_intercept=True,
-            intercept_scaling=1, loss='squared_hinge', max_iter=1000,
-            multi_class='ovr', penalty='l1', random_state=None, tol=0.0001,
-            verbose=0
-        )
-        for style in ('simple', 'term', 'vector')
-    }
-
-    simple_vecs = vectorizers['simple'].fit_transform([doc['text']
-                                                       for doc in train_docs])
-    term_vecs = vectorizers['term'].fit_transform([sklearn_text(doc['terms'])
-                                                   for doc in train_docs])
-    luminoso_vecs = normalize([unpack64(doc['vector'])
-                               for doc in train_docs])
-
-    classifiers['simple'].fit(simple_vecs, train_labels)
-    classifiers['term'].fit(term_vecs, train_labels)
-    classifiers['vector'].fit(luminoso_vecs, train_labels)
-
-    return (classifiers, vectorizers)
 
 
 def get_test_docs_from_file(filename, label_func=None):
@@ -250,19 +123,7 @@ def classify_test_documents(train_client, test_docs, test_labels, classifiers,
     '''
 
     test_docs = train_client.upload('docs/vectors', test_docs)
-    simple_vecs = vectorizers['simple'].transform([doc['text']
-                                                   for doc in test_docs])
-    term_vecs = vectorizers['term'].transform([sklearn_text(doc['terms'])
-                                               for doc in test_docs])
-    luminoso_vecs = normalize([unpack64(doc['vector'])
-                               for doc in test_docs])
-
-    classification = combine_decision_functions([
-        classifiers['simple'].decision_function(simple_vecs),
-        classifiers['term'].decision_function(term_vecs),
-        classifiers['vector'].decision_function(luminoso_vecs)
-    ],
-            len(classifiers['simple'].classes_))
+    classification = classify_documents(test_docs, classifiers, vectorizers)
 
     if save_results:
         results_dict = [dict({'text': z[0]['text'], 'truth': z[1]},
@@ -285,16 +146,7 @@ def return_label(new_text, classifiers, vectorizers, train_client):
     '''
 
     test_doc = train_client.upload('docs/vectors', [{'text': new_text}])[0]
-    simple_vecs = vectorizers['simple'].transform([test_doc['text']])
-    term_vecs = vectorizers['term'].transform([sklearn_text(test_doc['terms'])])
-    luminoso_vecs = normalize([unpack64(test_doc['vector'])])
-
-    classification = combine_decision_functions([
-        classifiers['simple'].decision_function(simple_vecs),
-        classifiers['term'].decision_function(term_vecs),
-        classifiers['vector'].decision_function(luminoso_vecs)
-    ],
-            len(classifiers['simple'].classes_))
+    classification = classify_documents([test_doc], classifiers, vectorizers)
 
     best_class = np.argmax(classification, axis=1)[0]
     return classifiers['simple'].classes_[best_class], classification[0][best_class]
@@ -367,9 +219,7 @@ def main(args):
                     *return_label(new_text, classifiers, vectorizers, train_client))
                       )
     else:
-        classifiers, vectorizers = train_classifier(
-            train_client, train_docs, train_labels
-            )
+        classifiers, vectorizers = train_classifier(train_docs, train_labels)
         classification = classify_test_documents(
             train_client, test_docs, test_labels, classifiers,
             vectorizers, args.save_results
