121fe053e639fb92716bb21ea84dfb75fe8db3e3
jlowryduda <joanna.teresa.duda@gmail.com>
2017-04-14 14:28:30 -0400

    Added an option to hide exact matches in conjunctions

--- se_code/conjunctions_disjunctions.py
+++ se_code/conjunctions_disjunctions.py
@@ -3,8 +3,8 @@ Example use: to get the document search results for new disjunction of the terms
 recommend' and 'not disappointed' in the Central Foods project, and hide the documents with the
 exact matches of either term, run:
 
-python conjunctions_disjunctions.py /projects/zoo/vfzct 'highly recommend' 'not disappointed'
---new --disjunction --docs --hide_exact
+python conjunctions_disjunctions.py zoo vfzct 'highly recommend' 'not disappointed' --new
+--disjunction --docs --hide-exact
 """
 
 from collections import defaultdict
@@ -17,8 +17,9 @@ from luminoso_api import LuminosoClient
 from se_code import fuzzy_logic
 
 
-def connect(project_id):
-    client = LuminosoClient.connect().change_path(project_id)
+def connect(account_id, project_id):
+    client = LuminosoClient.connect(
+        'https://analytics.luminoso.com/api/v4/projects/{}/{}'.format(account_id, project_id))
     return client
 
 
@@ -43,7 +44,7 @@ def get_current_results(client, search_terms, unit, n, hide_exact):
                     to_display = result['document']['text']
             else:
                 to_display = result['document']['text']
-        else:  # unit == 'search_terms'
+        else:  # unit == 'terms'
             to_display = result['text']
 
         if to_display:
@@ -52,47 +53,16 @@ def get_current_results(client, search_terms, unit, n, hide_exact):
             display_count += 1
 
 
-def get_new_conjunctions(client, terms, unit, n, function):
+def get_new_results(client, terms, unit, n, function, hide_exact):
     """
-    Given a list of terms, return the n documents or terms (unit) that the new solution would
-    return when supplied with these terms and an instruction to return conjunctions.
-    """
-    scores = defaultdict(lambda: len(terms) * [0])
+    Given a list of search terms, return the n documents or terms (unit) that the new solution
+    would return when supplied with these terms. It has an option to hide the documents
+    containing exact matches of the search terms (hide_exact=True).
 
-    for i, term in enumerate(terms):
-        search_results = client.get(unit + '/search', text=term, limit=10000)['search_results']
-        for result, matching_strength in search_results:
-            if unit == 'docs':
-                doc_id = result['document']['_id']
-            else:
-                doc_id = result['text']
-            scores[doc_id][i] = matching_strength
-
-    # Compute combined scores
-    final_scores = []
-    for doc_id, doc_scores in scores.items():
-        score = function(doc_scores)
-        final_scores.append((doc_id, score))
-
-    final_scores = sorted(final_scores, key=lambda x: x[1] if not np.isnan(x[1]) else 0,
-                          reverse=True)
-
-    # Print scores
-    for i, (doc_id, score) in enumerate(final_scores[:int(n)]):
-        if unit == 'docs':
-            text = client.get('/docs', id=doc_id)['text']
-        else:
-            text = doc_id
-        print(i + 1, text)
-        print()
-
-
-def get_new_disjunctions(client, terms, unit, n, function, hide_exact):
-    """
-    Given a list of terms, return the n documents or terms (unit) that the new solution would
-    return when supplied with these terms and an instruction to return conjunctions. This
-    function has an option to not show the documents with exact matches of either term (
-    hide_exact=True).
+    Depending on which function is supplied, get_new_results() will return either a conjunction
+    or a disjunction. Functions resulting in a conjunction are ex. fuzzy_logic.fuzzy_and or
+    fuzzy_logic.min_and. Functions resulting in a disjunction are ex. fuzzy_logic.fuzzy_or and
+    fuzzy_logic.max_and.
     """
     scores = defaultdict(lambda: len(terms) * [0])
 
@@ -141,6 +111,7 @@ def get_new_disjunctions(client, terms, unit, n, function, hide_exact):
 
 
 @click.command()
+@click.argument('account_id')
 @click.argument('project_id')
 @click.argument('search_terms', nargs=-1)
 @click.option('--current/--new', default=False, help='Get current or new results. Default: new')
@@ -149,19 +120,19 @@ def get_new_disjunctions(client, terms, unit, n, function, hide_exact):
 @click.option('--docs', 'unit', flag_value='docs', default=True, help='Get documents')
 @click.option('--terms', 'unit', flag_value='terms', help='Get terms')
 @click.option('--n', default=10, help='Number of results to show. Default: 10.')
-@click.option('--hide_exact', is_flag=True, help='Hide the documents including exact matches of '
+@click.option('--hide-exact', is_flag=True, help='Hide the documents including exact matches of '
                                                  'either one of the search terms. Default: False.')
-def main(project_id, search_terms, current, conjunction, unit, n, hide_exact):
-    client = connect(project_id)
+def main(account_id, project_id, search_terms, current, conjunction, unit, n, hide_exact):
+    client = connect(account_id, project_id)
 
     if current and conjunction:
         get_current_results(client, search_terms, unit, n, hide_exact)
     elif not current and conjunction:
-        get_new_conjunctions(client, search_terms, unit, n, fuzzy_logic.fuzzy_and)
+        get_new_results(client, search_terms, unit, n, fuzzy_logic.fuzzy_and, hide_exact)
     elif current and not conjunction:
         get_current_results(client, search_terms, unit, n, hide_exact)
     elif not current and not conjunction:
-        get_new_disjunctions(client, search_terms, unit, n, fuzzy_logic.fuzzy_or, hide_exact)
+        get_new_results(client, search_terms, unit, n, fuzzy_logic.fuzzy_or, hide_exact)
 
 if __name__ == '__main__':
     main()
