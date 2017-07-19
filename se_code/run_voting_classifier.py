ea2c52637104fe3097cfba152df87c3bb7718724
Dant86 <vedantdpathak@iCloud.com>
2017-07-19 18:03:07 +0000

    -_- There was a typo

--- se_code/run_voting_classifier.py
+++ se_code/run_voting_classifier.py
@@ -124,7 +124,7 @@ def classify_test_documents(train_client, test_docs, test_labels, classifiers,
     '''
 
     batched_test_docs = []
-    qtarter_size = int(len(test_docs) / 4)
+    quarter_size = int(len(test_docs) / 4)
     for i in range(0, len(test_docs), quarter_size):
         batched_test_docs.append(test_docs[i:i+quarter_size])
     for batch in batched_test_docs:
