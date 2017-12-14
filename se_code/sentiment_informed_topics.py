e1c71f59c193a8370223cb69691ff40c59e762f0
jlowryduda <joanna.teresa.duda@gmail.com>
2017-12-14 15:27:41 -0500

    more documentation

--- se_code/sentiment_informed_topics.py
+++ se_code/sentiment_informed_topics.py
@@ -2,6 +2,13 @@
 Example uses:
 python sentiment_informed_topics.py zoo rjcfz --sentiment pos --terms --verbose
 python sentiment_informed_topics.py zoo rjcfz --clusters --n-terms=1000
+
+Approach:
+Get top terms in the project equal to n_terms. Average the vectors for the terms which have a
+sentiment score assigned to them by SentimentScorer, taking into account that sentiment score and
+the term's relevance. Use a sentiment axis created in this way to query the project for similar
+terms. Sort the terms using their new sentiment score (how well they match the sentiment axis)
+and their relevance. Return the number of top domain-specific terms equal to n_results.
 """
 
 import json
@@ -17,9 +24,7 @@ from scipy.stats import hmean
 class SentimentTopics:
     def __init__(self, account_id, project_id, language, n_terms):
         self.client = self._connect(account_id, project_id)
-        self.sentiment_scorer = SentimentScorer(language)
-        self.n_terms = n_terms
-        self.project_terms = self._get_project_terms()
+        self.project_terms = self._get_project_terms(n_terms, language)
         self.axes = {}
         self.domain_sentiment_terms = {}
 
@@ -29,22 +34,26 @@ class SentimentTopics:
             'https://analytics.luminoso.com/api/v4/projects/{}/{}'.format(account_id, project_id))
         return client
 
-    def _get_project_terms(self):
+    def _get_project_terms(self, n_terms, language):
         """
         Get the top n terms in the project. Assign a sentiment score (orig-sentiment-score) to each
         term according to the sentiment scorer. Unpack each term's vector and overwrite its
         'vector' field.
         """
-        terms = self.client.get('terms', limit=self.n_terms)
+        sentiment_scorer = SentimentScorer(language)
+        terms = self.client.get('terms', limit=n_terms)
         terms = [term for term in terms if term['vector']]
         for term in terms:
             term['vector'] = unpack64(term['vector'])
-            term['orig-sentiment-score'] = self.sentiment_scorer.term_sentiment(term['term'])
+            term['orig-sentiment-score'] = sentiment_scorer.term_sentiment(term['term'])
         return terms
 
     def _get_known_sentiment_terms(self, sentiment):
         """
-        Get the terms from which a sentiment axis will be built.
+        Get the terms from which a sentiment axis will be built. For positive sentiment,
+        these are the terms with the original sentiment score (assigned by SentimentScorer) of
+        over 0. For negative sentiment, these are terms with the original sentiment score of less
+        than 0.
         """
         if sentiment == 'pos':
             return [term for term in self.project_terms if term['orig-sentiment-score'] > 0]
@@ -53,7 +62,10 @@ class SentimentTopics:
 
     def _get_sent_axis(self, sentiment):
         """
-        Compute the sentiment axis. #TODO more documentation
+        Return the sentiment axis for a specified sentiment. The sentiment axis is computed using
+        the terms in the project that have a sentiment scored assigned to them by SentimentScorer
+        (orig-sentiment-score). Their vectors are averaged, weighted by the orig-sentiment-score
+        as well as the relevance to the project (score).
         """
         if sentiment in self.axes:
             return self.axes[sentiment]
@@ -92,27 +104,31 @@ class SentimentTopics:
 
     def sorted_sentiment_terms(self, sentiment, n_results):
         """
-        Get the project terms that best match the specified sentiment axis. Each
-        term is assigned a sentiment score (how well it matched the axis) and a relevance score. The
-        terms are then sorted using these two scores.
+        Get the project terms that best match the specified sentiment axis. Normalize their
+        sentiment and relevance scores. Return the list of top n_results domain-specific terms,
+        sorted by the harmonic mean of their sentiment and relevance scores.
         """
         sentiment_terms = self.get_domain_sentiment_terms(sentiment, n_results)
-
-        all_sentiment_scores = []
-        all_relevance_scores = []
-        for term in sentiment_terms:
-            all_sentiment_scores.append(term['new-sentiment-score'])
-            all_relevance_scores.append(term['score'])
-
-        for term in sentiment_terms:
-            term['norm-new-sent-score'] = self._normalize(term['new-sentiment-score'],
-                                                          all_sentiment_scores)
-            term['norm-relevance-score'] = self._normalize(term['score'], all_relevance_scores)
-
+        self._normalize_scores(sentiment_terms)
         return sorted(sentiment_terms, key=self._harmonic_mean, reverse=True)[:n_results]
 
+    def _normalize_scores(self, terms):
+        """
+        Normalize sentiment score (new-sentiment-score) and relevance score (score) of each term
+        to a value between 0 and 1.
+        """
+        all_sentiment_scores = []
+        all_relevance_scores = []
+        for term in terms:
+            all_sentiment_scores.append(term['new-sentiment-score'])
+            all_relevance_scores.append(term['score'])
+        for term in terms:
+            term['norm-new-sent-score'] = self._normalize_score(term['new-sentiment-score'],
+                                                                all_sentiment_scores)
+            term['norm-relevance-score'] = self._normalize_score(term['score'], all_relevance_scores)
+
     @staticmethod
-    def _normalize(score, all_scores):
+    def _normalize_score(score, all_scores):
         """
         Normalize the scores to be between 0 and 1
         """
@@ -172,8 +188,9 @@ def print_sentiment_terms(terms, verbose=False):
 @click.option('--language', '-l', default='en')
 @click.option('--terms', is_flag=True, help='Use to get a list of sentiment terms')
 @click.option('--clusters', is_flag=True, help='Cluster only the sentiment terms')
-@click.option('--n-terms', default=1000, help='Number of project terms to operate on. More terms '
-                                             'usually means more accurate results.')
+@click.option('--n-terms', default=1000, help='Number of project terms among which to find terms '
+                                              'with original sentiment score. This list will be '
+                                              'used to create a sentiment axis.')
 @click.option('--n-results', default=30, help='Number of results to show')
 @click.option('--n-clusters', default=7, help='Show clusters')
 @click.option('--verbose', '-v', is_flag=True, help='Show details about the results')
