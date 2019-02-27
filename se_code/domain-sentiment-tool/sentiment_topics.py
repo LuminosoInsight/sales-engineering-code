84e87eaf894b5ac7d830188473f1d104d7a59c62
William Wood Harter <wharter@luminoso.com>
2019-02-26 20:39:39 -0800

    Fixed the non-comment comment which should have just been removed

--- se_code/domain-sentiment-tool/sentiment_topics.py
+++ se_code/domain-sentiment-tool/sentiment_topics.py
@@ -35,13 +35,13 @@ class SentimentTopics:
         term according to the sentiment scorer. Unpack each term's vector and overwrite its
         'vector' field.
         """
-        language = self.client.get(fields=['language'])['language']
+        language = self.client.get(fields=["language"])["language"]
         sentiment_scorer = SentimentScorer(language)
-        terms = self.client.get('terms', limit=n_terms)
-        terms = [term for term in terms if term['vector']]
+        terms = self.client.get("terms", limit=n_terms)
+        terms = [term for term in terms if term["vector"]]
         for term in terms:
-            term['vector'] = [float(v) for v in unpack64(term['vector'])] // unpack64(term['vector'])
-            term['orig-sentiment-score'] = sentiment_scorer.term_sentiment(term['term'])
+            term["vector"] = [float(v) for v in unpack64(term["vector"])]
+            term["orig-sentiment-score"] = sentiment_scorer.term_sentiment(term["term"])
         return terms
 
     def _get_known_sentiment_terms(self, sentiment):
@@ -51,10 +51,14 @@ class SentimentTopics:
         over 0. For negative sentiment, these are terms with the original sentiment score of less
         than 0.
         """
-        if sentiment == 'pos':
-            return [term for term in self.project_terms if term['orig-sentiment-score'] > 0]
-        elif sentiment == 'neg':
-            return [term for term in self.project_terms if term['orig-sentiment-score'] < 0]
+        if sentiment == "pos":
+            return [
+                term for term in self.project_terms if term["orig-sentiment-score"] > 0
+            ]
+        elif sentiment == "neg":
+            return [
+                term for term in self.project_terms if term["orig-sentiment-score"] < 0
+            ]
 
     def _get_sent_axis(self, sentiment):
         """
@@ -67,12 +71,17 @@ class SentimentTopics:
             return self.axes[sentiment]
 
         sent_terms = self._get_known_sentiment_terms(sentiment)
-        axis = sent_terms[0]['vector'] * 0
-        axis_sum = sum(abs(term['orig-sentiment-score']) for term in sent_terms)
+        axis = sent_terms[0]["vector"] * 0
+        axis_sum = sum(abs(term["orig-sentiment-score"]) for term in sent_terms)
         for term in sent_terms:
-            axis += term['vector'] * abs(term['orig-sentiment-score']) * term['score'] / axis_sum
+            axis += (
+                term["vector"]
+                * abs(term["orig-sentiment-score"])
+                * term["score"]
+                / axis_sum
+            )
         print(str(axis))
-        print("dot="+str(axis.dot(axis)))
+        print("dot=" + str(axis.dot(axis)))
         axis /= (axis.dot(axis)) ** 0.5
 
         self.axes[sentiment] = axis
@@ -89,14 +98,16 @@ class SentimentTopics:
 
         axis = self._get_sent_axis(sentiment)
         terms = self.client.get(
-            'terms/search', vector=pack64(axis),
-            limit=self.n_results if self.n_results > 200 else 200)['search_results']
+            "terms/search",
+            vector=pack64(axis),
+            limit=self.n_results if self.n_results > 200 else 200,
+        )["search_results"]
 
         sentiment_terms = []
         for term, matching_strength in terms:
-            term['new-sentiment-score'] = matching_strength
-            term['vector'] = [float(v) for v in unpack64(term['vector'])] // unpack64(term['vector'])
-            term['sentiment-label'] = sentiment
+            term["new-sentiment-score"] = matching_strength
+            term["vector"] = [float(v) for v in unpack64(term["vector"])]
+            term["sentiment-label"] = sentiment
             sentiment_terms.append(term)
 
         self.domain_sentiment_terms[sentiment] = sentiment_terms
@@ -110,19 +121,24 @@ class SentimentTopics:
         """
         sentiment_terms = self.get_domain_sentiment_terms(sentiment)
         self._normalize_scores(sentiment_terms)
-        return sorted(sentiment_terms, key=self._harmonic_mean, reverse=True)[:self.n_results]
+        return sorted(sentiment_terms, key=self._harmonic_mean, reverse=True)[
+            : self.n_results
+        ]
 
     def _normalize_scores(self, terms):
         """
         Normalize sentiment score (new-sentiment-score) and relevance score (score) of each term
         to a value between 0 and 1.
         """
-        all_sentiment_scores = [term['new-sentiment-score'] for term in terms]
-        all_relevance_scores = [term['score'] for term in terms]
+        all_sentiment_scores = [term["new-sentiment-score"] for term in terms]
+        all_relevance_scores = [term["score"] for term in terms]
         for term in terms:
-            term['norm-new-sent-score'] = self._normalize_score(term['new-sentiment-score'],
-                                                                all_sentiment_scores)
-            term['norm-relevance-score'] = self._normalize_score(term['score'], all_relevance_scores)
+            term["norm-new-sent-score"] = self._normalize_score(
+                term["new-sentiment-score"], all_sentiment_scores
+            )
+            term["norm-relevance-score"] = self._normalize_score(
+                term["score"], all_relevance_scores
+            )
 
     @staticmethod
     def _normalize_score(score, all_scores):
@@ -137,18 +153,18 @@ class SentimentTopics:
         If the norm-new-sent-score and the norm-relevance-score are positive, return their
         harmonic mean. Otherwise, return 0. The secondary sorting key is norm-new-sent-score.
         """
-        scores = (term['norm-new-sent-score'], term['norm-relevance-score'])
+        scores = (term["norm-new-sent-score"], term["norm-relevance-score"])
         if all(scores):  # harmonic mean is defined
-            return hmean(scores), term['norm-new-sent-score']
+            return hmean(scores), term["norm-new-sent-score"]
         else:
-            return 0, term['norm-new-sent-score']
+            return 0, term["norm-new-sent-score"]
 
     def cluster_sentiment_terms(self):
         """
         Get 200 positive terms and 200 negative terms, and cluster them.
         """
-        pos_terms = self.get_domain_sentiment_terms('pos')[:200]
-        neg_terms = self.get_domain_sentiment_terms('neg')[:200]
+        pos_terms = self.get_domain_sentiment_terms("pos")[:200]
+        neg_terms = self.get_domain_sentiment_terms("neg")[:200]
         tree = ClusterTree.from_term_list(pos_terms + neg_terms)
         return tree
 
@@ -159,9 +175,9 @@ def print_clusters(tree, n_clusters=7):
     """
     for subtree in tree.flat_cluster_list(n_clusters):
         subtree_terms = [term for term in subtree.filtered_termlist[:3]]
-        print(subtree.term['text'])
+        print(subtree.term["text"])
         for term in subtree_terms:
-            print('{} '.format(term['text']))
+            print("{} ".format(term["text"]))
         print()
 
 
@@ -171,37 +187,56 @@ def print_sentiment_terms(terms, verbose=False):
     relevance score.
     """
     if verbose:
-        print('Term\tSentiment score\tRelevance score')
+        print("Term\tSentiment score\tRelevance score")
     for term in terms:
-        output = '{}'.format(term['text'])
+        output = "{}".format(term["text"])
         if verbose:
-            output += '\t{}\t{}'.format(term['norm-new-sent-score'], term['norm-relevance-score'])
+            output += "\t{}\t{}".format(
+                term["norm-new-sent-score"], term["norm-relevance-score"]
+            )
         print(output)
 
 
 @click.command()
-@click.argument('account_id')
-@click.argument('project_id')
-@click.option('--api', default='https://analytics.luminoso.com/api/v4/projects')
-@click.option('--sentiment', default='pos', help='pos, neg')
-@click.option('--terms', is_flag=True, help='Use to get a list of sentiment terms')
-@click.option('--clusters', is_flag=True, help='Cluster only the sentiment terms')
-@click.option('--n-terms', default=500, help='Number of project terms among which to find terms '
-                                              'with original sentiment score. This list will be '
-                                              'used to create a sentiment axis.')
-@click.option('--n-results', default=30, help='Number of results to show')
-@click.option('--n-clusters', default=7, help='Show clusters')
-@click.option('--verbose', '-v', is_flag=True, help='Show details about the results')
-def main(account_id, project_id, api, sentiment, terms, clusters, n_terms, n_results, n_clusters,
-         verbose):
-    client = LuminosoClient.connect('{}/{}/{}'.format(api, account_id, project_id))
+@click.argument("account_id")
+@click.argument("project_id")
+@click.option("--api", default="https://analytics.luminoso.com/api/v4/projects")
+@click.option("--sentiment", default="pos", help="pos, neg")
+@click.option("--terms", is_flag=True, help="Use to get a list of sentiment terms")
+@click.option("--clusters", is_flag=True, help="Cluster only the sentiment terms")
+@click.option(
+    "--n-terms",
+    default=500,
+    help="Number of project terms among which to find terms "
+    "with original sentiment score. This list will be "
+    "used to create a sentiment axis.",
+)
+@click.option("--n-results", default=30, help="Number of results to show")
+@click.option("--n-clusters", default=7, help="Show clusters")
+@click.option("--verbose", "-v", is_flag=True, help="Show details about the results")
+def main(
+    account_id,
+    project_id,
+    api,
+    sentiment,
+    terms,
+    clusters,
+    n_terms,
+    n_results,
+    n_clusters,
+    verbose,
+):
+    client = LuminosoClient.connect("{}/{}/{}".format(api, account_id, project_id))
     sentiment_topics = SentimentTopics(client, n_terms, n_results)
 
     if terms:
-        print_sentiment_terms(sentiment_topics.sorted_sentiment_terms(sentiment), verbose=verbose)
+        print_sentiment_terms(
+            sentiment_topics.sorted_sentiment_terms(sentiment), verbose=verbose
+        )
 
     if clusters:
         print_clusters(sentiment_topics.cluster_sentiment_terms(), n_clusters)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
