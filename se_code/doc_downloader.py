846f3f32d70bc59fb566640599765ac9ba98155c
William Wood Harter <wharter@luminoso.com>
2020-10-09 13:15:39 -0700

    Added sentiment for saved tags for each document

--- se_code/doc_downloader.py
+++ se_code/doc_downloader.py
@@ -1,5 +1,5 @@
 from luminoso_api import V5LuminosoClient as LuminosoClient
-import csv, json, datetime, time, argparse
+import collections, csv, json, datetime, time, argparse
 
 def get_all_docs(client):
     docs = []
@@ -22,15 +22,27 @@ def search_all_doc_ids(client, concepts,match_type="both"):
         else:
             return docs
 
-def add_relations(client,docs,add_concept_relations=False,add_concept_list=False,match_type="both"):
+def add_sentiment_to_saved_concepts(client, saved_concepts):
+    saved_concept_ids = [{"saved_concept_id":c['saved_concept_id']} for c in saved_concepts]
+    concept_selector = {"type": "specified", "concepts": saved_concept_ids}
+    saved_sentiments = client.get('concepts/sentiment', concept_selector=concept_selector)
+    for sc, match_count in zip(saved_concepts, saved_sentiments['match_counts']):
+        sc['sentiment_share'] = match_count['sentiment_share']
+        # get the highest sentiment share and use that for sentiment
+        sc['sentiment'] = collections.Counter(match_count['sentiment_share']).most_common()[0][0]
+
+def add_relations(client, docs, add_concept_relations=False, add_concept_list=False, match_type="both", add_match_score=False, add_saved_concept_sentiment=False):
     # get the list of saved concepts
     concept_selector = {"type": "saved"}
     saved_concepts = client.get('concepts', concept_selector=concept_selector)['result']
+    add_sentiment_to_saved_concepts(client, saved_concepts)
 
+    sentiment_texts = []
     for sc in saved_concepts:
         sc['match_scores'] = search_all_doc_ids(client,sc['texts'],match_type=match_type)
         sc['match_scores_by_id'] = {c['doc_id']:c['match_score'] for c in sc['match_scores']}
         sc['docs_ids'] = [c['doc_id'] for c in sc['match_scores']]
+        sentiment_texts
 
     # filter out metadata that matches our current saved concepts
     clist = [sc['name'] for sc in saved_concepts]
@@ -49,13 +61,19 @@ def add_relations(client,docs,add_concept_relations=False,add_concept_list=False
                 v = 'yes'
                 if add_concept_relations:
                     d['metadata'].append({'name':sc['name'],'type':'string','value':v})
-                    d['metadata'].append({'name':sc['name']+' match_score','type':'number','value':sc['match_scores_by_id'][d['doc_id']]})
+                    if add_match_score:
+                        d['metadata'].append({'name':sc['name']+' match_score','type':'number','value':sc['match_scores_by_id'][d['doc_id']]})
                 doc_concept_list.append(sc['name'])
+
+                if add_saved_concept_sentiment:
+                    d['metadata'].append({'name':sc['name']+' sentiment','type':'string','value':sc['sentiment']})
             else:
                 v = 'no'
                 if add_concept_relations:
                     d['metadata'].append({'name':sc['name'],'type':'string','value':v})
-                    d['metadata'].append({'name':sc['name']+' match_score','type':'number','value':0})
+                    if add_match_score:
+                        d['metadata'].append({'name':sc['name']+' match_score','type':'number','value':0})
+
 
         if add_concept_relations:
             if in_none:
@@ -121,10 +139,23 @@ def main():
     parser.add_argument('filename', help="Name of CSV file to write project documents to")
     parser.add_argument('-e', '--encoding', default='utf-8', help="Encoding type of the file to write to")
     parser.add_argument('-d', '--date_format', default='%Y-%m-%d', help="Format of timestamp")
+
     parser.add_argument('-c', '--concept_relations', default=False, action='store_true', help="Add columns for saved concept relations and outliers")
     parser.add_argument('-l', '--concept_list', default=False, action='store_true', help="Add columns for saved concept relations and outliers")
-    parser.add_argument('-m', '--match_type', default='both', help="For concept relations use exact, conceptual or both when searching")
+    parser.add_argument('-mt', '--match_type', default=None, help="For concept relations use exact, conceptual or both when searching")
+    parser.add_argument('-ms', '--match_score', default=False, action='store_true', help="For concept relations also include the match_score")
+    parser.add_argument('-s', '--concept_relations_sentiment', default=False, action="store_true", help="Add saved concept sentiment to the concept_relations")
+    
     args = parser.parse_args()
+
+    # require that -c is set if using any of the other flags associated with concept relations
+    if (args.concept_list or args.match_score or args.concept_relations_sentiment or args.match_type!=None) and (not args.concept_relations):
+        parser.error("-l, -mt, -ms, -s all require -c")
+
+    # set the default value for match type. Not using this in add_argument, because
+    # need to know if user set for checking that -c is also set
+    if not args.match_type:
+        args.match_type = 'both'
 
     api_url = args.project_url.split('/app')[0]
     project_id = args.project_url.strip('/ ').split('/')[-1]
@@ -138,9 +169,14 @@ def main():
 
     docs = get_all_docs(client)
     if args.concept_relations or args.concept_list:
-        add_relations(client,docs,args.concept_relations,args.concept_list,match_type=args.match_type)
+        add_relations(client,docs,args.concept_relations,args.concept_list,match_type=args.match_type,add_match_score=args.match_score, add_saved_concept_sentiment=args.concept_relations_sentiment)
     fields = get_fields(docs)
     docs, field_names = format_subsets(docs, fields, args.date_format)
+
+    # need a better sort to the field names
+    field_name_dict = {fn:fn.replace("string_","").replace("number_","").replace("score_","").replace("text","1text").replace("title","1title") for fn in field_names}
+    field_names = {k: v for k, v in sorted(field_name_dict.items(), key=lambda item: item[1])}.keys()
+
     write_to_csv(args.filename, docs, field_names, encoding=args.encoding)
 
 
