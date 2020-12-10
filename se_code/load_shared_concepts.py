355a81d79f49b7c774279ad9c6f792807186796e
William Wood Harter <wharter@luminoso.com>
2020-12-10 11:06:17 -0800

    Improved help output on load_shared_concepts

--- se_code/load_shared_concepts.py
+++ se_code/load_shared_concepts.py
@@ -2,24 +2,54 @@ import argparse
 import csv
 import json
 
+from argparse import RawTextHelpFormatter
+
 from luminoso_api import V5LuminosoClient as LuminosoClient
 from se_code.copy_shared_concepts import delete_shared_concepts
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description='Upload a file containing shared concepts to a target Daylight project.'
+        description=('Upload a file containing shared concepts '
+                     'to a target Daylight project.'
+                     '\n\nExample csv:'
+                     '\n concept_list_name,texts, name, color'
+                     '\n lista,"tablet,ipad",tablet,#808080'
+                     '\n lista,device,device,#800080'
+                     '\n lista,speakers, speakers,#800000'
+                     '\n listb,"usb,usb-c,micro-usb","cable","#008080"'
+                     '\n listb,"battery","battery","#000080"'
+                     '\n listb,"switch","switch","#808000"'
+                     '\n\nExample JSON:'
+                     '\n [{"concept_list_name":"lista"'
+                     '\n   "concepts": ['
+                     '\n    {"texts":"tablet,ipad","name":"tablet","color":"#808080"},'
+                     '\n    {"texts":"device", "name":"device","color":"#800080"},'
+                     '\n    {"texts":"speakers", "name":"speakers","color":"#800000"}]},'
+                     '\n  {"concept_list_name":"listb",'
+                     '\n  "concepts": ['
+                     '\n    {"texts":"usb,usb-c,micro-usb","name":"cable","color":"#008080"},'
+                     '\n    {"texts":"battery", "name":"battery","color":"#000080"},'
+                     '\n    {"texts":"switch", "name":"switch","color":"#808000"}]}'
+                     '\n ]'), 
+        formatter_class=RawTextHelpFormatter
     )
-    parser.add_argument('filename', help="Full name of the file containing shared concept definitions, including the extension. Must be CSV or JSON.")
-    parser.add_argument('project_url', help="Full URL of the project to load the concepts into.")
-    parser.add_argument('-d', '--delete', default=False, action="store_true", help="Whether to delete all the existing shared concepts or not.")
-    parser.add_argument('-e', '--encoding', default="utf-8", help="Encoding type of the files to read from")
+    parser.add_argument(
+        'filename',
+        help=('Full name of the file containing shared concept definitions,'
+              ' including the extension. Must be CSV or JSON.'
+)
+        )
+    parser.add_argument('-d', '--delete', default=False, action="store_true", 
+        help="Whether to delete all the existing shared concepts or not.")
+    parser.add_argument('-e', '--encoding', default="utf-8", 
+        help="Encoding type of the files to read from")
     args = parser.parse_args()
-    
+
     root_url = args.project_url.strip('/ ').split('/app')[0]
     project_id = args.project_url.strip('/').split('/')[6]
     client = LuminosoClient.connect(url=root_url + '/api/v5/projects/' + project_id)
-    
+
     filename = args.filename
     true_data = []
     if '.csv' in filename:
