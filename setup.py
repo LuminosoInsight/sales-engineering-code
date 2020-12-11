b094e4e3b0000b2d6ae858e36cb5a328200ec3bb
William Wood Harter <wharter@luminoso.com>
2020-12-10 22:51:02 -0800

    improvements to the wheel build to exclude internal scripts

--- setup.py
+++ setup.py
@@ -16,8 +16,21 @@ setup(
         ],
     entry_points={
         'console_scripts': [
-            'lumi-add-concept-relations-to_project = se_code.add_concept_relations_to_project:main',
-            'lumi-copy-shared-concepts = se_code.copy_shared_concepts:main',
-            'lumi-doc-downloader = se_code.doc_downloader:main'
+            'lumi_add_concept_relations_to_project = se_code.add_concept_relations_to_project:main',
+            'lumi_capitalize_saved_concepts = se_code.capitalize_saved_concepts:main',
+            'lumi_copy_shared_concepts = se_code.copy_shared_concepts:main',
+            'lumi_create_train_test_split.py = se_code.create_train_test_split:main',
+            'lumi_doc_downloader = se_code.doc_downloader:main',
+            'lumi_format_multiple_text_fields = se_code.format_multiple_text_fields:main',
+            'lumi_get_all_scoredrivers = se_code.get_all_score_drivers:main',
+            'lumi_ignore_terms = se_code.ignore_terms:main',
+            'lumi_list_metadata = se_code.list_metadaa:main',
+            'lumi_load_shared_concepts = se_code.load_shared_concepts:main',
+            'lumi_onsite_list_users = se_code.onsite_list_users:main',
+            'lumi_onsite_usage = se_code.onsite_usage:main',
+            'lumi_project_migration = se_code.project_migration:main',
+            'lumi_score_drivers = se_code.score_drivers:main',
+            'lumi_bi_tools_export = se_code.bi_tools_export',
+
         ]},
 )
