e2ff5ecc78f403ccad0652a3405ed0bab0f0a341
William Wood Harter <wharter@luminoso.com>
2021-01-22 14:46:56 -0800

    Fix for csv files that have columns with the same name

--- se_code/format_multiple_text_fields.py
+++ se_code/format_multiple_text_fields.py
@@ -1,13 +1,33 @@
 import csv
 import argparse
 
+
 def file_to_dict(file_name, encoding="utf-8"):
     table = []
     with open(file_name, encoding=encoding) as f:
-        reader = csv.DictReader(f)
+        reader = csv.reader(f)
+
+        # found some csv files have same name columns
+        # map and change those to unique names so can still use as dict
+        raw_header = next(reader)
+        header_map = {}
+        header = []
+        for name in raw_header:
+            if name in header:
+                idx = 2
+                new_name = "{}_{}".format(name, idx)
+                while new_name in header:
+                    idx += 1
+                    new_name = "{}_{}".format(name, idx)
+                header_map[new_name] = name
+
+                name = new_name
+            header.append(name)
+
         for row in reader:
-            table.append(row)
-    return table
+            table.append(dict(zip(header, row)))
+    return table, header_map
+
 
 def file_to_list(file_name, encoding="utf-8"):
     table = []
@@ -16,26 +36,35 @@ def file_to_list(file_name, encoding="utf-8"):
         for row in reader:
             table.append(row)
     return table
-    
-def dict_to_file(table, file_name, encoding="utf-8"):
+
+
+def dict_to_file(table, file_name, encoding="utf-8", header_map={}):
     fields = []
     for key in table[0]:
-        fields.append(key)
+        if key in header_map:
+            fields.append(header_map[key])
+        else:
+            fields.append(key)
+
     with open(file_name, 'w', newline='', encoding=encoding) as f:
-        writer = csv.DictWriter(f, fields)
-        writer.writeheader()
-        writer.writerows(table)
-        
+        writer = csv.writer(f)
+        writer.writerow(fields)
+        for row in table:
+            writer.writerow(row.values())
+
+
 def list_to_file(table, file_name, encoding="utf-8"):
     with open(file_name, 'w', newline='', encoding=encoding) as f:
         writer = csv.writer(f)
         writer.writerows(table)
-        
+
+
 def char_position(letters):
     index = 0
     for i, char in enumerate(letters):
         index += ((ord(char.lower()) - 97) + (i * 26))
     return index
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -61,7 +90,7 @@ def main():
     args = parser.parse_args()
     
     write_table = []
-    table = file_to_dict(args.input_file, encoding=args.encoding)
+    table, header_map = file_to_dict(args.input_file, encoding=args.encoding)
     text_fields = [field for field in table[0] if 'text_' in field.lower() or 'text' == field.lower().strip()]
     for read_row in table:
         for key in read_row:
@@ -73,7 +102,8 @@ def main():
                 else:
                     write_row.update({'string_' + args.column_dest: 'Text'})
                 write_table.append(write_row)
-    dict_to_file(write_table, args.output_file, encoding=args.encoding)
-    
+    dict_to_file(write_table, args.output_file, encoding=args.encoding, header_map=header_map)
+
+
 if __name__ == '__main__':
     main()
