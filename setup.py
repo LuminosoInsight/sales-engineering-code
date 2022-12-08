0d2b40713918acb002a8d9aef54c07607a860d46
William Wood Harter <wharter@luminoso.com>
2022-12-08 12:00:02 -0800

    version update 0.9.2 to fix typo in wheel file setup/install

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="se_code",
-    version='0.9.1',
+    version='0.9.2',
     maintainer='Luminoso Technologies, Inc.',
     maintainer_email='dev@luminoso.com',
     license="LICENSE",
